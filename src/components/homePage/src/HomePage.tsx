0040ce0b582cd597a2ea45773c9447244fc0fb86
Rachel Khan <rachel.khan@valtech.com>
2020-04-22 17:02:14 +0100

    add home page component

---
+++ src/components/homePage/src/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Delete me
+export const HomePage = () => {
+	return (
+		<>
+			<div>Ultraflix Homepage</div>
+		// want the header and the footer on the page
+		</>
+	);
+};
---
+++ src/components/homePage/stories/HomePage.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HomePage } from '../src';
+
+export default {
+	title: 'HomePage',
+	component: HomePage
+};
+
+export const Story = () => {
+	return <HomePage />;
+};
