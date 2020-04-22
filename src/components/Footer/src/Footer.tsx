406390b64b77f94ec4e9aae1c6ecb19ffc65b1e0
Thomas Williams <thomas.williams@valtech.com>
2020-04-22 11:12:07 +0100

    Import content from contentful

--- src/components/Footer/src/Footer.tsx
+++ src/components/Footer/src/Footer.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useContentful, HookResponse } from 'react-contentful';
+// import './Footer.scss';
 
-export const Footer = () => (
-	<div className="app-content-container">
-		<div>Text</div>
-		<div>Logo</div>
-		<div>More Text</div>
-	</div>
-);
+export const Footer: React.FC<{}> = () => {
+	const footerContent: HookResponse = useContentful({
+		id: '4psLuIzH33I6TptbtKJkVX',
+	});
+
+	if (footerContent.loading || !footerContent.fetched) {
+		return null;
+	}
+
+	if (footerContent.error) {
+		console.error(footerContent.error);
+		return null;
+	}
+
+	if (!footerContent.data) {
+		return <p>Page does not exist.</p>;
+	}
+
+	return (
+		<div className="footer">
+			<div>{footerContent.data['fields'].copyrightText}</div>
+			<img
+				src={footerContent.data['fields'].logo['fields'].file.url}
+				alt={footerContent.data['fields'].logo['fields'].description}
+			/>
+			<div>{footerContent.data['fields'].companyText}</div>
+		</div>
+	);
+};
