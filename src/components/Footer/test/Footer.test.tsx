d8c19ed1069f6d397a15905b3d127dc4ca540354
Thomas Williams <thomas.williams@valtech.com>
2020-04-23 15:09:51 +0100

    Adding tests

--- src/components/Footer/test/Footer.test.tsx
+++ src/components/Footer/test/Footer.test.tsx
@@ -1,11 +1,111 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import 'jest-extended';
-import { Footer } from '../src';
+import { Footer, FooterProps } from '../src';
+import { ContentfulImage } from '../../../types';
 
-// describe('{{componentName}} Component', () => {
-// 	it('renders without crashing', () => {
-// 		render(<Footer />);
-// 	});
-// });
+describe('Footer Component', () => {
+	let footerContent: Partial<FooterProps>;
+	let logo: Partial<ContentfulImage>;
+
+	beforeEach(() => {
+		logo = {
+			url: 'logo_url',
+			description: 'Logo Description',
+		};
+
+		footerContent = {
+			copyrightText: 'Test copyright text',
+			logo: logo as ContentfulImage,
+			companyText: 'Test company text',
+		};
+	});
+
+	describe('When copyrightText is undefined', () => {
+		it('Returns null', () => {
+			// Arrange
+			footerContent.copyrightText = undefined;
+
+			// Act
+			const { container } = render(
+				<Footer {...(footerContent as FooterProps)} />
+			);
+
+			// Assert
+			expect(container).toBeEmpty();
+		});
+	});
+
+	describe('When logo is undefined', () => {
+		it('Returns null', () => {
+			// Arrange
+			footerContent.logo = undefined;
+
+			// Act
+			const { container } = render(
+				<Footer {...(footerContent as FooterProps)} />
+			);
+
+			// Assert
+			expect(container).toBeEmpty();
+		});
+	});
+
+	describe('When logo.url is undefined', () => {
+		it('Returns null', () => {
+			// Arrange
+			footerContent.logo!.url = (undefined as unknown) as string;
+
+			// Act
+			const { container } = render(
+				<Footer {...(footerContent as FooterProps)} />
+			);
+
+			// Assert
+			expect(container).toBeEmpty();
+		});
+	});
+
+	describe('When logo.description is undefined', () => {
+		it('Returns null', () => {
+			// Arrange
+			footerContent.logo!.description = (undefined as unknown) as string;
+
+			// Act
+			const { container } = render(
+				<Footer {...(footerContent as FooterProps)} />
+			);
+
+			// Assert
+			expect(container).toBeEmpty();
+		});
+	});
+
+	describe('When companyText is undefined', () => {
+		it('Returns null', () => {
+			// Arrange
+			footerContent.companyText = undefined;
+
+			// Act
+			const { container } = render(
+				<Footer {...(footerContent as FooterProps)} />
+			);
+
+			// Assert
+			expect(container).toBeEmpty();
+		});
+	});
+
+	describe('When all content is defined', () => {
+		it('Returns null', () => {
+			// Act
+			const { container } = render(
+				<Footer {...(footerContent as FooterProps)} />
+			);
+
+			// Assert
+			expect(container).toHaveTextContent(footerContent.copyrightText!);
+			expect(container).toHaveTextContent(footerContent.companyText!);
+		});
+	});
+});
