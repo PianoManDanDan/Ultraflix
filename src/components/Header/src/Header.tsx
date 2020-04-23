302020b06eefe2629e002bbf27b85eba55ec9acb
Daniel Martin <djm_djm@hotmail.com>
2020-04-23 15:05:30 +0100

    Add props null check to header

--- src/components/Header/src/Header.tsx
+++ src/components/Header/src/Header.tsx
@@ -7,6 +7,9 @@ export type HeaderProps = {
 };
 
 export const Header: React.FC<HeaderProps> = ({ logo }) => {
+	if (!logo) {
+		return null;
+	}
 	return (
 		<header className="header">
 			<a className="header__logo-link" href="/">
