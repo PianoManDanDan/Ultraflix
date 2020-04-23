b288d2523470ab605fe4b1d00311da39c6a623cb
Thomas Williams <thomas.williams@valtech.com>
2020-04-23 10:17:20 +0100

    Moving to HTTPS

---
+++ src/components/Footer/test/Footer.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import 'jest-extended';
+import { Footer } from '../src';
+
+// describe('{{componentName}} Component', () => {
+// 	it('renders without crashing', () => {
+// 		render(<Footer />);
+// 	});
+// });
