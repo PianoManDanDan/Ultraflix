81ea41842bc8aa8ccb864f5eda17463543947cf9
Daniel Martin <djm_djm@hotmail.com>
2020-04-27 12:18:40 +0100

    Add Header to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { HookResponse, useContentful } from 'react-contentful';
+import { Header, HeaderProps } from '../components/Header/src';
 import { Footer, FooterProps } from '../components/Footer/src';
+
+const getHeaderContent = (headerContent): HeaderProps => {
+	return {
+		logo: {
+			url: headerContent.data['fields'].logo['fields'].file.url,
+			description: headerContent.data['fields'].logo['fields'].description,
+		},
+	};
+};
 
 const getFooterContent = (footerContent): FooterProps => {
 	return {
@@ -14,11 +24,21 @@ const getFooterContent = (footerContent): FooterProps => {
 };
 
 export const HomePage: React.FC<{}> = () => {
+	const headerContent: HookResponse = useContentful({
+		id: '4V1H6JAO1iUCB0a9RW1kIs',
+	});
 	const footerContent: HookResponse = useContentful({
 		id: '4psLuIzH33I6TptbtKJkVX',
 	});
 
-	if (footerContent.loading || !footerContent.fetched || !footerContent.data) {
+	if (
+		headerContent.loading ||
+		headerContent.fetched ||
+		!headerContent.data ||
+		footerContent.loading ||
+		!footerContent.fetched ||
+		!footerContent.data
+	) {
 		return null;
 	}
 
@@ -27,5 +47,10 @@ export const HomePage: React.FC<{}> = () => {
 		return null;
 	}
 
-	return <Footer {...getFooterContent(footerContent)} />;
+	return (
+		<div>
+			<Header {...getHeaderContent(headerContent)} />
+			<Footer {...getFooterContent(footerContent)} />
+		</div>
+	);
 };
