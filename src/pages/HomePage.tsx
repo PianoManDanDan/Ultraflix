2256b602a3ee5fe9caf09fee5dec71aa3a55b8b7
Daniel Martin <djm_djm@hotmail.com>
2020-05-05 17:11:59 +0100

    Remove need for getContent functions and use mappers in component utils

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
 import { HookResponse, useContentful } from 'react-contentful';
 import { Header } from '../components/Header/src';
-import { HeaderProps } from '../components/Header/src/types';
+import { headerPropsMapper } from '../components/Header/utils';
 import { Footer } from '../components/Footer/src';
-import { FooterProps } from '../components/Footer/src/types';
-
-const getHeaderContent = (headerContent): HeaderProps => {
-	return {
-		logo: {
-			url: headerContent.data['fields'].logo['fields'].file.url,
-			description: headerContent.data['fields'].logo['fields'].description,
-		},
-	};
-};
-
-const getFooterContent = (footerContent): FooterProps => {
-	return {
-		copyrightText: footerContent.data['fields'].copyrightText,
-		logo: {
-			url: footerContent.data['fields'].logo['fields'].file.url,
-			description: footerContent.data['fields'].logo['fields'].description,
-		},
-		companyText: footerContent.data['fields'].companyText,
-	};
-};
+import { footerPropsMapper } from '../components/Footer/src/utils';
 
 export const HomePage: React.FC<{}> = () => {
 	const headerContent: HookResponse = useContentful({
@@ -56,9 +36,9 @@ export const HomePage: React.FC<{}> = () => {
 
 	return (
 		<div className="app-page-container">
-			<Header {...getHeaderContent(headerContent)} />
+			<Header {...headerPropsMapper(headerContent)} />
 			<div className="app-page-content-container" />
-			<Footer {...getFooterContent(footerContent)} />
+			<Footer {...footerPropsMapper(footerContent)} />
 		</div>
 	);
 };
