2d9b3941551b4629701ad8092e768751032f6adc
Daniel Martin <djm_djm@hotmail.com>
2020-05-05 16:11:31 +0100

    Update WhatsOn story

--- src/components/WhatsOn/stories/WhatsOn.stories.tsx
+++ src/components/WhatsOn/stories/WhatsOn.stories.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+	ContentfulClient,
+	ContentfulClientInterface,
+	ContentfulProvider,
+} from 'react-contentful';
 import { WhatsOn } from '../src';
 
 export default {
@@ -7,5 +12,13 @@ export default {
 };
 
 export const Story = () => {
-	return <WhatsOn />;
+	const contentfulClient: ContentfulClientInterface = ContentfulClient({
+		accessToken: 'im2Om8TMefwnoIwcZsknuuh3mKseOiNucWFLwjsJKYg',
+		space: 'v9yjf2k31vk7',
+	});
+	return (
+		<ContentfulProvider client={contentfulClient}>
+			<WhatsOn />
+		</ContentfulProvider>
+	);
 };
