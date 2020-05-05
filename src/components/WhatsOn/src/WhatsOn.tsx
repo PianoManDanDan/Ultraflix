060d3e7d168f5676114b65f95d472406112c8746
Daniel Martin <djm_djm@hotmail.com>
2020-05-05 16:11:22 +0100

    Add contentful data to WhatsOn Component

--- src/components/WhatsOn/src/WhatsOn.tsx
+++ src/components/WhatsOn/src/WhatsOn.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import { SubThing } from './subcomponents/Carousel';
+import { useContentful, HookResponse } from 'react-contentful';
+import { MovieSlide } from './subcomponents/MovieSlide/src';
+import { MovieSlideProps } from './subcomponents/MovieSlide/src/types';
+import { MovieSlidePropsMapper } from './utils';
+// import { SubThing } from './subcomponents/Carousel';
 
 export const WhatsOn: React.FC<{}> = () => {
+	const filmListContent: HookResponse = useContentful({
+		id: '3eClse9t6aIwURH79xlUAK',
+	});
+
+	if (
+		filmListContent.loading ||
+		!filmListContent.fetched ||
+		!filmListContent.data
+	) {
+		return null;
+	}
+
+	if (filmListContent.error) {
+		console.error(filmListContent.error);
+		return null;
+	}
+
+	console.error(filmListContent);
+
+	const mappedContent: MovieSlideProps[] = MovieSlidePropsMapper(
+		filmListContent.data['fields'].movies
+	);
+
 	return (
 		<div>
 			<p>What&apos;s On in the cinema</p>
-			<SubThing />
+			<MovieSlide {...mappedContent[0]} />
+			<MovieSlide {...mappedContent[1]} />
+			<MovieSlide {...mappedContent[2]} />
 		</div>
 	);
 };
