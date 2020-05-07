c8de604d0a906e35e9829666f526bf66003f25a3
Daniel Martin <djm_djm@hotmail.com>
2020-05-07 12:46:07 +0100

    Finish tests over WhatsOn Component

--- src/components/WhatsOn/test/WhatsOn.test.tsx
+++ src/components/WhatsOn/test/WhatsOn.test.tsx
@@ -2,46 +2,93 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-extended';
+import { useContentful } from 'react-contentful';
 import { WhatsOn } from '../src';
+import { Carousel } from '../src/subcomponents/Carousel/src';
+
+jest.mock('react-contentful');
+jest.mock('../src/subcomponents/Carousel/src');
 
 describe('WhatsOn Component', () => {
 	afterEach(() => {
-		jest.clearAllMocks();
+		jest.resetAllMocks();
 	});
 
 	it('renders the correct heading', () => {
 		// Arrange
 		const mockHeading = 'test Heading';
-		jest.mock('react-contentful', () => ({
-			useContentful: jest.fn(() => {
-				return {
-					fetched: true,
-					loading: true,
-					data: {
-						fields: {
-							heading: mockHeading,
-							movies: null,
+		useContentful.mockReturnValue({
+			fetched: true,
+			loading: false,
+			data: {
+				fields: {
+					heading: mockHeading,
+					movies: [
+						{
+							fields: {
+								posterImage: {
+									fields: {
+										file: {
+											url: '',
+										},
+										description: '',
+									},
+								},
+								title: '',
+								runtime: '',
+								certificate: '',
+								releaseYear: 0,
+							},
 						},
-					},
-				};
-			}),
-
-			// .mockReturnValue({
-			// 	fetched: true,
-			// 	loading: true,
-			// 	data: {
-			// 		fields: {
-			// 			heading: mockHeading,
-			// 			movies: null,
-			// 		},
-			// 	},
-			// }),
-		}));
+					],
+				},
+			},
+		});
+		Carousel.mockReturnValue('');
 
 		// Act
 		const { container } = render(<WhatsOn />);
 
 		// Assert
 		expect(container).toHaveTextContent(mockHeading);
+	});
+
+	it('renders the Carousel component', () => {
+		// Arrange
+		const mockCarousel = 'Carousel Component';
+		useContentful.mockReturnValue({
+			fetched: true,
+			loading: false,
+			data: {
+				fields: {
+					heading: '',
+					movies: [
+						{
+							fields: {
+								posterImage: {
+									fields: {
+										file: {
+											url: '',
+										},
+										description: '',
+									},
+								},
+								title: '',
+								runtime: '',
+								certificate: '',
+								releaseYear: 0,
+							},
+						},
+					],
+				},
+			},
+		});
+		Carousel.mockReturnValue(mockCarousel);
+
+		// Act
+		const { container } = render(<WhatsOn />);
+
+		// Assert
+		expect(container).toHaveTextContent(mockCarousel);
 	});
 });
