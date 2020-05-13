b87a052c4458f351e50d651ee23f7619e5cbc005
Thomas Williams <thomas.williams@valtech.com>
2020-05-13 12:23:58 +0100

    Changes based on CR feedback

---
+++ src/globalStyles/_sizes.scss.d.ts
@@ -0,0 +1,14 @@
+type sizesScss = {
+	xsMinBreakpoint;
+	xsMaxBreakpoint;
+	smMinBreakpoint;
+	smMaxBreakpoint;
+	mdMinBreakpoint;
+	mdMaxBreakpoint;
+	lgMinBreakpoint;
+	lgMaxBreakpoint;
+};
+
+const sizes: sizesScss;
+
+export default sizes;
