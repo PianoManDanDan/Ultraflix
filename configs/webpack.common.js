7a332bfed59899633002e2570c6d395f416b314d
Daniel Martin <Daniel.Martin@valtech.com>
2020-05-27 12:58:07 +0100

    Remove react and react-dom from webpack bundle

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -94,8 +94,8 @@ module.exports = {
 		],
 	},
 
-	// externals: {
-	// 	'react': 'React',
-	// 	'react-dom': 'ReactDOM',
-	// },
+	externals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM',
+	},
 };
