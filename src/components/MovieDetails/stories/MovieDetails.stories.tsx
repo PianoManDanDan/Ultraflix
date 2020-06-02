2f4afcc3a1b3a3c755b26f9fec40b2d8f764ef3b
Daniel Martin <Daniel.Martin@valtech.com>
2020-06-02 13:12:28 +0100

    Add Storybook story for MovieDetails component

--- src/components/MovieDetails/stories/MovieDetails.stories.tsx
+++ src/components/MovieDetails/stories/MovieDetails.stories.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { text, number, select } from '@storybook/addon-knobs';
 import { MovieDetails } from '../src';
+
+const getMovieProps = (): MovieProps => ({
+	posterImage: {
+		url: '/movie_slide_image.jpg',
+		description: 'Movie Poster Image',
+	},
+	title: text('Film Title', 'Happy Feet 2'),
+	rating: number('Rating', 3),
+	runtime: text('Runtime', '2 HR 4 MIN'),
+	certificate: select(
+		'Certificate',
+		{ U: 'U', PG: 'PG', Twelve: '12A', Fifteen: '15', Eighteen: '18' },
+		'PG'
+	),
+	releaseYear: number('Release Year', 2020),
+});
 
 export default {
 	title: 'MovieDetails',
@@ -7,4 +24,5 @@ export default {
 };
 
 export const Story = () => {
+	return <MovieDetails {...getMovieProps} />;
 };
