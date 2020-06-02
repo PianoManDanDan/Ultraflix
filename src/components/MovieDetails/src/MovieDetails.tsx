007c29fa554e64a5ac22f60f10f102b110bb7b36
Daniel Martin <Daniel.Martin@valtech.com>
2020-06-02 13:11:01 +0100

    Add content to MovieDetails component

--- src/components/MovieDetails/src/MovieDetails.tsx
+++ src/components/MovieDetails/src/MovieDetails.tsx
@@ -1,3 +1,22 @@
 import React from 'react';
+import Rater from 'react-rater';
+import { Image } from '../../Image/src';
 import './MovieDetails.scss';
 
+export const MovieDetails: React.FC<MovieProps> = ({
+	posterImage,
+	title,
+	rating,
+	runtime,
+}) => {
+	return (
+		<div>
+			<Image {...posterImage} />
+			<h2 className="movie-title">{title}</h2>
+			<Rater total={5} rating={rating} interactive={false} />
+			<p className="runtime">{runtime}</p>
+
+			{/* <MovieDetails /> */}
+		</div>
+	);
+};
