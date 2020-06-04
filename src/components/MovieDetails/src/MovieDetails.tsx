ac1eccbbe3682ec3a6275ebbd9378373943149f9
Daniel Martin <Daniel.Martin@valtech.com>
2020-06-04 15:28:50 +0100

    Add content containers and update styling

--- src/components/MovieDetails/src/MovieDetails.tsx
+++ src/components/MovieDetails/src/MovieDetails.tsx
@@ -15,21 +15,31 @@ export const MovieDetails: React.FC<Movie> = ({
 	rating,
 }) => {
 	return (
-		<div>
-			<Image className="movie-details-image" {...posterImage} />
-			<h2 className="movie-details-title">
-				{title} ({releaseYear})
-			</h2>
-			<Rater total={5} rating={rating} interactive={false} />
-			<p className="movie-details-certificate">{certificate}</p>
-			<p className="movie-details-runtime">{runtime}</p>
-
-			<p className="movie-details-description">{description}</p>
+		<div className="movie-details app-content-container">
+			<div className="row">
+				<div className="col-xs-12 col-md-2">
+					<Image className="movie-details__image" {...posterImage} />
+				</div>
+				<div className="col-xs-12 col-md-10">
+					<h2 className="movie-details__title">
+						{title} ({releaseYear})
+					</h2>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-xs-12 col-md-2">
 					<Rater
 						total={5}
 						rating={ratingRoundHalf(rating)}
 						interactive={false}
 					/>
+					<p className="movie-details__certificate">{certificate}</p>
+					<p className="movie-details__runtime">{runtime}</p>
+				</div>
+				<div className="col-xs-12 col-md-10">
+					<p className="movie-details__description">{description}</p>
+				</div>
+			</div>
 		</div>
 	);
 };
