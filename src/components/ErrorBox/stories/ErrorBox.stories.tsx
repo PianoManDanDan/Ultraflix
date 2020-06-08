c6b761bddb6b1724cb642a63a821b46f4ed7277d
Thomas Williams <thomas.williams@valtech.com>
2020-06-08 13:47:43 +0100

    Adding storybook knobs

--- src/components/ErrorBox/stories/ErrorBox.stories.tsx
+++ src/components/ErrorBox/stories/ErrorBox.stories.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { number, text } from '@storybook/addon-knobs';
 import { ErrorBox, ErrorBoxProps } from '../src';
 
 const getErrorBoxProps = (): ErrorBoxProps => ({
-	errorCode: 404,
-	heading: 'Oops something went wrong',
-	message: 'This page does not exist.',
+	errorCode: number('Error Code', 404),
+	heading: text('Error heading', 'Oops something went wrong'),
+	message: text('Error message', 'This page does not exist.'),
 });
 
 export default {
