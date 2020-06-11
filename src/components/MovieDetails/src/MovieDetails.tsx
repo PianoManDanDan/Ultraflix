cb7be86b7e4b478145bd7bbb248f7c27a074b16f
Daniel Martin <Daniel.Martin@valtech.com>
2020-06-11 13:53:51 +0100

    Fix styling

--- src/components/MovieDetails/src/MovieDetails.tsx
+++ src/components/MovieDetails/src/MovieDetails.tsx
@@ -18,32 +18,34 @@ export const MovieDetails: React.FC<Movie> = ({
 		return null;
 	}
 	return (
-		<div className="movie-details app-content-container">
-			<div className="row">
-				<div className="col-xs-12 col-md-2">
-					<Image className="movie-details__image" {...posterImage} />
+		<div className="app-content-container">
+			<div className="movie-details">
+				<div className="row">
+					<div className="col-xs-12 col-md-2">
+						<Image className="movie-details__image" {...posterImage} />
+					</div>
+					<div className="col-xs-12 col-md-10 movie-details__title">
+						{title} {releaseYear ? `(${releaseYear})` : ''}
+					</div>
 				</div>
-				<div className="col-xs-12 col-md-10 movie-details__title">
-					{title} {releaseYear ? `(${releaseYear})` : ''}
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-xs-12 col-md-2">
-					{rating ? (
-						<Rater
-							total={5}
-							rating={ratingRoundHalf(rating)}
-							interactive={false}
-						/>
-					) : (
-						''
-					)}
+				<div className="row">
+					<div className="col-xs-12 col-md-2">
+						{rating ? (
+							<Rater
+								total={5}
+								rating={ratingRoundHalf(rating)}
+								interactive={false}
+							/>
+						) : (
+							''
+						)}
 
-					<p className="movie-details__certificate">{certificate || ''}</p>
-					<p className="movie-details__runtime">{runtime || ''}</p>
-				</div>
-				<div className="col-xs-12 col-md-10">
-					<p className="movie-details__description">{description || ''}</p>
+						<p className="movie-details__certificate">{certificate || ''}</p>
+						<p className="movie-details__runtime">{runtime || ''}</p>
+					</div>
+					<div className="col-xs-12 col-md-10">
+						<p className="movie-details__description">{description || ''}</p>
+					</div>
 				</div>
 			</div>
 		</div>
