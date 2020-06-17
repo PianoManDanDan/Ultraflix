5590aa196713fe3aea25d66ba5793afb8c23e3eb
Daniel Martin <Daniel.Martin@valtech.com>
2020-06-17 13:00:19 +0100

    Stop 404 page being shown between redirect and contentful  data coming back

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -3,19 +3,14 @@ import { Header, useGetContentfulHeader } from '../components/Header/src';
 import { Footer, useGetContentfulFooter } from '../components/Footer/src';
 import { MovieDetails } from '../components/MovieDetails/src';
 import { useGetContentfulMovie } from '../utils';
-import { NotFoundPage } from '.';
 
 export const MovieDetailsPage = ({ match }) => {
 	const headerContent = useGetContentfulHeader('4V1H6JAO1iUCB0a9RW1kIs');
 	const footerContent = useGetContentfulFooter('4psLuIzH33I6TptbtKJkVX');
 	const movie = useGetContentfulMovie(match.params.movieContentfulID);
 
-	if (!headerContent || !footerContent) {
+	if (!headerContent || !footerContent || !movie) {
 		return null;
-	}
-
-	if (!movie) {
-		return <NotFoundPage />;
 	}
 
 	if (movie.title) {
