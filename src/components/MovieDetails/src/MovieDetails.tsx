1ef4ed2ce6fe0dc96d0569f2cac82c9ca1c63278
Daniel Martin <Daniel.Martin@valtech.com>
2020-06-29 13:20:37 +0100

    Update styling

--- src/components/MovieDetails/src/MovieDetails.tsx
+++ src/components/MovieDetails/src/MovieDetails.tsx
@@ -20,33 +20,39 @@ export const MovieDetails: React.FC<Movie> = ({
 	return (
 		<div className="app-content-container">
 			<div className="movie-details">
-				<div className="row">
-					<div className="col-xs-12 col-md-2">
-						<Image className="movie-details__image" {...posterImage} />
-					</div>
-					<div className="col-xs-12 col-md-10">
-						<span className="movie-details__title">
-							{title} {releaseYear ? `(${releaseYear})` : ''}
-						</span>
+				<div className="movie-details-head">
+					<div className="row">
+						<div className="col-xs-12 col-md-2">
+							<Image className="movie-details__image" {...posterImage} />
+						</div>
+						<div className="col-xs-12 col-md-10">
+							<span className="movie-details__title">
+								{title} {releaseYear ? `(${releaseYear})` : ''}
+							</span>
+						</div>
 					</div>
 				</div>
-				<div className="row">
-					<div className="col-xs-12 col-md-2">
-						{rating || rating === 0 ? (
-							<Rater
-								total={5}
-								rating={halveAndRound(rating)}
-								interactive={false}
-							/>
-						) : (
-							''
-						)}
+				<div className="movie-details-content">
+					<div className="row">
+						<div className="col-xs-12 col-md-2">
+							{rating || rating === 0 ? (
+								<Rater
+									total={5}
+									rating={halveAndRound(rating)}
+									interactive={false}
+								/>
+							) : (
+								// <div className="movie-details__no-rating">No rating yet</div>
+								''
+							)}
 
-						<div className="movie-details__certificate">{certificate}</div>
-						<div className="movie-details__runtime">{runtime}</div>
-					</div>
-					<div className="col-xs-12 col-md-10">
-						<div className="movie-details__description">{description}</div>
+							<div className="movie-details__certificate-runtime">
+								{certificate}&emsp;|&emsp;{runtime}
+							</div>
+						</div>
+						<div className="col-xs-12 col-md-10">
+							<div className="movie-details__description">{description}</div>
+						</div>
 					</div>
 				</div>
 			</div>
