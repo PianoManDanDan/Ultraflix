8ce0b45d7818a324ddb052d1bebd4b2a4ecfd8d0
Daniel Martin <Daniel.Martin@valtech.com>
2020-07-08 17:06:04 +0100

    Edit styling and layout

--- src/components/MovieDetails/src/MovieDetails.tsx
+++ src/components/MovieDetails/src/MovieDetails.tsx
@@ -20,40 +20,32 @@ export const MovieDetails: React.FC<Movie> = ({
 	return (
 		<div className="app-content-container">
 			<div className="movie-details">
-				<div className="row">
-					<div className="movie-details-head">
-						<div className="col-xs-12 col-md-2">
-							<Image className="movie-details__image" {...posterImage} />
-						</div>
-						<div className="col-xs-12 col-md-10">
-							<span className="movie-details__title">
-								{title} {releaseYear ? `(${releaseYear})` : ''}
-							</span>
+				<div className="col-xs-12 col-md-2" id="movie-details__sidebar">
+					<Image className="movie-details__image" {...posterImage} />
+					<div className="movie-details__rating">
+						{rating || rating === 0 ? (
+							<Rater
+								total={5}
+								rating={halveAndRound(rating)}
+								interactive={false}
+							/>
+						) : (
+							<div className="movie-details__no-rating">No rating yet</div>
+						)}
+
+						<div className="movie-details__certificate-runtime">
+							{certificate}&emsp;|&emsp;{runtime}
 						</div>
 					</div>
 				</div>
-				<div className="movie-details-content">
-					<div className="row">
-						<div className="col-xs-12 col-md-2">
-							{rating || rating === 0 ? (
-								<Rater
-									total={5}
-									rating={halveAndRound(rating)}
-									interactive={false}
-								/>
-							) : (
-								// <div className="movie-details__no-rating">No rating yet</div>
-								''
-							)}
+				<div className="col-xs-12 col-md-10">
+					<span className="movie-details__title">
+						{title} {releaseYear ? `(${releaseYear})` : ''}
+					</span>
+				</div>
 
-							<div className="movie-details__certificate-runtime">
-								{certificate}&emsp;|&emsp;{runtime}
-							</div>
-						</div>
-						<div className="col-xs-12 col-md-10">
-							<div className="movie-details__description">{description}</div>
-						</div>
-					</div>
+				<div className="col-xs-12 col-md-10" id="movie-details__description">
+					{description}
 				</div>
 			</div>
 		</div>
